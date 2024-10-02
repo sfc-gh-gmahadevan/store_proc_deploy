15c64eb455159a715e65341619f22454ba68a7dc
Ganesh Mahadevan <ganesh.mahadevan@snowflake.com>
2024-10-02 08:17:05 -0500

    update proc

--- NonPII/SilverRaw_synapse/DimLoader/Dim.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE GMAHADEVAN_DB.GMAHADEVAN_SCHEMA.DimAccountingDocumentHeader()
-    RETURNS VARCHAR(16777216)
-    LANGUAGE PYTHON
-    RUNTIME_VERSION = '3.10'
-    PACKAGES = ('snowflake-snowpark-python')
-    HANDLER = 'DimAccountingDocumentHeader.main'
-    IMPORTS = ('@GMAHADEVAN_DB.GMAHADEVAN_SCHEMA.GIT_STORED_PROC_REPO/branches/dev/NonPII/SilverRaw_synapse/DimLoader/DimAccountingDocumentHeader.py')
-    EXECUTE AS OWNER
-    ;
