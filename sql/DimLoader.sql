fd0af61a5aa563d94dfaeb069128bf5a1ad6f2b4
Ganesh Mahadevan <ganesh.mahadevan@snowflake.com>
2024-10-02 15:23:02 -0500

    test var

--- sql/DimLoader.sql
+++ sql/DimLoader.sql
@@ -1,19 +1,19 @@
-CREATE OR REPLACE PROCEDURE GMAHADEVAN_DB.GMAHADEVAN_SCHEMA.DimAccountingDocumentHeader()
+CREATE OR REPLACE PROCEDURE <% DB_NAME %>.<% SCHEMA_NAME %>.DimAccountingDocumentHeader()
     RETURNS VARCHAR(16777216)
     LANGUAGE PYTHON
     RUNTIME_VERSION = '3.10'
     PACKAGES = ('snowflake-snowpark-python')
     HANDLER = 'DimAccountingDocumentHeader.main'
-    IMPORTS = ('@GIT_STORED_PROC_REPO/branches/main/NonPII/SilverRaw_synapse/DimLoader/DimAccountingDocumentHeader.py')
+    IMPORTS = ('@<% DB_NAME %>.<% SCHEMA_NAME %>.GIT_STORED_PROC_REPO/branches/main/NonPII/SilverRaw_synapse/DimLoader/DimAccountingDocumentHeader.py')
     EXECUTE AS OWNER
     ;
 
-CREATE OR REPLACE PROCEDURE GMAHADEVAN_DB.GMAHADEVAN_SCHEMA.DimActionPlanEHS()
+CREATE OR REPLACE PROCEDURE <% DB_NAME %>.<% SCHEMA_NAME %>.DimActionPlanEHS()
     RETURNS VARCHAR(16777216)
     LANGUAGE PYTHON
     RUNTIME_VERSION = '3.10'
     PACKAGES = ('snowflake-snowpark-python')
     HANDLER = 'DimActionPlanEHS.main'
-    IMPORTS = ('@GIT_STORED_PROC_REPO/branches/main/NonPII/SilverRaw_synapse/DimLoader/DimActionPlanEHS.py')
+    IMPORTS = ('@<% DB_NAME %>.<% SCHEMA_NAME %>.GIT_STORED_PROC_REPO/branches/main/NonPII/SilverRaw_synapse/DimLoader/DimActionPlanEHS.py')
     EXECUTE AS OWNER
     ;
