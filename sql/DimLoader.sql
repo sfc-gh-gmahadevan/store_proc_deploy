d5c1337098d72d557ea3dfd998658eafa3b1db8c
Ganesh Mahadevan <ganesh.mahadevan@snowflake.com>
2024-10-03 11:19:28 -0500

    test package

--- sql/DimLoader.sql
+++ sql/DimLoader.sql
@@ -1,3 +1,13 @@
+CREATE OR REPLACE PROCEDURE <% DB_NAME %>.<% SCHEMA_NAME %>.DimWell1()
+    RETURNS VARCHAR(16777216)
+    LANGUAGE PYTHON
+    RUNTIME_VERSION = '3.11'
+    PACKAGES = ('snowflake-snowpark-python', '@GMAHADEVAN_DB.GMAHADEVAN_SCHEMA.SUNCOR_LIB/helper_class_2.zip')
+    HANDLER = 'DimWell.main'
+    IMPORTS = ('@<% DB_NAME %>.<% SCHEMA_NAME %>.GIT_STORED_PROC_REPO/branches/feature_1/NonPII/SilverRaw_synapse/DimLoader/DimWell.py')
+    EXECUTE AS OWNER
+    ;
+
 CREATE OR REPLACE PROCEDURE <% DB_NAME %>.<% SCHEMA_NAME %>.DimAccountingDocumentHeader()
     RETURNS VARCHAR(16777216)
     LANGUAGE PYTHON
